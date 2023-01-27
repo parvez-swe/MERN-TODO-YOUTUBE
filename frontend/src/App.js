4e69a3832e14865d79092288f27e5c5b959c5ef9
Parvez <mdparvezmusharaf2@gmail.com>
2023-01-27 14:58:01 +0600

    ad

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,32 +35,35 @@ function App() {
   };
 
   return (
-    <div className="bg-gray-300 min-h-[100vh]">
-      <form
-        className="bg-[#FF0 000] mx-5 py-2 sticky flex top-0 justify-between rounded-xl"
-        onSubmit={updating ? UpdatesubmitHandler : AddsubmitHandler}
-      >
-        <input
-          className="px-2 mx-2 outline-none w-[90%] rounded-lg"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button className="text-white px-2 font-bold">
-          {updating ? "Update" : "Add"}
-        </button>
-      </form>
-
-      {todo.map((item) => (
-        <Todo
-          key={item._id}
-          text={item.text}
-          completed={item.completed}
-          updateMode={() => updateMode(item._id, item.text)}
-          completToDo={() => completToDo(item._id, setTodo)}
-          deleteToDo={() => deleteToDo(item._id, setTodo)}
-        />
-      ))}
-    </div>
+    <>
+      <div className="bg-gray-300 min-h-[100vh]">
+        <h1 className="text-center font-bold text-xl">To Do App</h1>
+        <form
+          className="bg-[#FF0 000] mx-5 py-2 sticky flex top-0 justify-between rounded-xl"
+          onSubmit={updating ? UpdatesubmitHandler : AddsubmitHandler}
+        >
+          <input
+            className="px-2 mx-2 outline-none w-[90%] rounded-lg"
+            placeholder="Text Here..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button className="bg-black px-4 rounded-xl py-2 [#FF0000] text-white px-2 font-bold">
+            {updating ? "Update" : "Add"}
+          </button>
+        </form>
+        {todo.map((item) => (
+          <Todo
+            key={item._id}
+            text={item.text}
+            completed={item.completed}
+            updateMode={() => updateMode(item._id, item.text)}
+            completToDo={() => completToDo(item._id, setTodo)}
+            deleteToDo={() => deleteToDo(item._id, setTodo)}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
